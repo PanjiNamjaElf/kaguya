b818a997a683062a0b4bd94a68b78023a8785920
Panji Setya Nur Prawira <panjinamjaelf@users.noreply.github.com>
2020-06-07 05:56:53 +0700

    Remove laravel ui

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,15 +6,11 @@
 
 namespace PanjiNamjaElf\Kaguya\Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Ui\UiServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use PanjiNamjaElf\Kaguya\KaguyaServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
-
     /**
      * Get package providers.
      *
@@ -25,7 +21,6 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
-            UiServiceProvider::class,
             KaguyaServiceProvider::class,
         ];
     }
@@ -41,9 +36,10 @@ abstract class TestCase extends BaseTestCase
 
         $this->app->setBasePath(__DIR__ . '/../');
 
-        $this->artisan('migrate', ['--database' => 'testing']);
-
-        $this->app->make('router')->auth();
+        $this->loadMigrationsFrom([
+            '--database' => 'testing',
+            '--path'     => realpath(__DIR__ . '/database/migrations'),
+        ]);
     }
 
     /**
@@ -56,7 +52,6 @@ abstract class TestCase extends BaseTestCase
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.debug', env('APP_DEBUG', true));
-        $app['config']->set('database.default', 'testing');
         $app['config']->set('logging.channels.single.path', __DIR__ . '/../logs/kaguya_' . date('Y.m.d_His') . '.log');
     }
 }
